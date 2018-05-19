a65dbe633cd4adef5e4f66788627861170182d41
Cletus Lee <clet.lee@gmail.com>
2018-05-19 17:31:59 +0800

    initial version

---
+++ src/main/java/home/cletus/rds/RdsPocApplication.java
@@ -0,0 +1,12 @@
+package home.cletus.rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdsPocApplication.class, args);
+	}
+}
