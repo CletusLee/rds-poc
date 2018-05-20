bf3d31d4b32d8c4d4a507605c8ad08d0cc2fad3f
Cletus Lee <clet.lee@gmail.com>
2018-05-20 22:57:25 +0800

    Adjusted the request mapping

--- src/main/java/home/cletus/rds/RdsPocApplication.java
+++ src/main/java/home/cletus/rds/RdsPocApplication.java
@@ -1,12 +1,24 @@
 package home.cletus.rds;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RdsPocApplication {
+
+	@Value("${my.profile}")
+	private String myProperty;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RdsPocApplication.class, args);
+	}
+
+	@RequestMapping("/profile")
+	public String helloWorld() {
+		return "Hello World[" + myProperty + "]!!";
 	}
 }
--- src/main/java/home/cletus/rds/controller/CustomerController.java
+++ src/main/java/home/cletus/rds/controller/CustomerController.java
@@ -1,17 +1,31 @@
 package home.cletus.rds.controller;
 
+import home.cletus.rds.entity.Customer;
+import home.cletus.rds.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/customer")
 public class CustomerController {
 
-    @Value("${my.profile}")
-    private String myProperty;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-    @RequestMapping("/")
-    public String helloWorld() {
-        return "Hello World[" + myProperty + "]!!";
+    @PutMapping("{id}")
+    public ResponseEntity addCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        customer = customerRepository.save(customer);
+
+        return new ResponseEntity(customer, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Integer id) {
+        Customer c = customerRepository.findById(id).get();
+        return new ResponseEntity(c, HttpStatus.OK);
     }
 }
