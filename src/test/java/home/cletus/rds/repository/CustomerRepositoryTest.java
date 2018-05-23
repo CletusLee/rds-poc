49ff80b625bbaa30d05aaeeef208401034ed168e
Cletus Lee <clet.lee@gmail.com>
2018-05-23 19:58:11 +0800

    changed to use TestEntityManager to test the behavior of Spring Data. If we save the data through repository objects, we are just testing against cached data.

--- src/test/java/home/cletus/rds/repository/CustomerRepositoryTest.java
+++ src/test/java/home/cletus/rds/repository/CustomerRepositoryTest.java
@@ -5,32 +5,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
-//@DataJpaTest
-//@RunWith(SpringRunner.class)
-//@Transactional
+@DataJpaTest
+@RunWith(SpringRunner.class)
+@Transactional
 public class CustomerRepositoryTest {
 
-//    @Autowired
-//    CustomerRepository customerRepository;
+    @Autowired
+    CustomerRepository customerRepository;
 
-//    @Test
-//    public void saveData() {
-//        Customer c = new Customer();
-//        c.setName("Cletus");
-//        c.setAge(25);
-//
-//        c = customerRepository.save(c);
-//
-//        assertNotNull(c.getId());
-//    }
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
-    public void emptyTest() {
+    public void saveData() {
+        Customer c = new Customer();
+        c.setName("Cletus");
+        c.setAge(25);
 
+        Customer saveEntity = entityManager.persistFlushFind(c);
+        Customer result = customerRepository.findById(saveEntity.getId()).get();
+
+        assertNotNull(result.getId());
     }
 }
